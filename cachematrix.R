f519ad7540688c15ce500261b94f6f39e88978ff
sayan126 <banerjees.chem@gmail.com>
2016-07-08 15:55:38 +0530

    First Commit by sayan126

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,86 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+makeCacheMatrix <- function(x=matrix())
+{
+	x <- NULL
+	inv <- NULL
+	set <- function(maty=matrix())
+	{
+		x <<- maty
+		data <<- x
+		inv <<- NULL
+	}
+
+	get <- function()
+	{
+		data
+	}
+
+	setinv <- function(invNew)
+	{ 
+		inv <<- invNew
+	}
+
+	getinv <- function()
+	{
+		inv
+	} 	
+
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
+
+## Write a short comment describing this code section
+
+functionPointer <- makeCacheMatrix()
+data <- NULL
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...)
+{
+	inv <- functionPointer$getinv()
+	compare <- data == x
+	flag <- "oldData"
+	if(!is.null(data) && nrow(data)==nrow(x) && ncol(data)==ncol(x))
+	{
+		for(i in seq_along(nrow(data)))
+		{
+			for(j in seq_along(ncol(data)))
+			{
+				if(compare[i][j]=="FALSE")
+				{
+					data <<- x
+					flag <- "newData"
+					cat("New matrix dataset has been entered: " , "\n")
+					print(data)
+					cat("\n")
+					break
+				}
+			}	
+		}
+
+	}
+
+	if(!is.null(inv) && flag=="oldData")
+	{
+		message("getting cached data")
+		cat("Matrix Dataset = ","\n")
+		print(data)
+		cat("Matrix Inverse = ","\n")
+		return(inv)
+	}
+
+	functionPointer$set(x)
+	cat("New matrix dataset has been set", "\n")
+	inv <- solve(data,...)
+	functionPointer$setinv(inv)
+	cat("Inverse for new matrix = ","\n")
+	return(inv)
 }
+
+
+
